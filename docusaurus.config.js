8b39ef67aa2b312ce738bca879cf11ffe7aba15f
Omar Ezzat <omar.ezzat@dataport.de>
2023-09-05 10:11:09 +0200

    add build folder

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://hpi-schul-cloud.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hpi-schul-cloud', // Usually your GitHub org/user name.
   projectName: 'hpi-schul-cloud', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
