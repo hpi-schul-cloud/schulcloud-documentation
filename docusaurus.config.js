b16c91ed956b84a9c297b6db31e903cb336cc835
Omar Ezzat <omar.ezzat@dataport.de>
2023-09-13 12:21:08 +0200

    change repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://hpi-schul-cloud.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/schulcloud-documentation',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hpi-schul-cloud', // Usually your GitHub org/user name.
-  projectName: 'hpi-schul-cloud.github.io', // Usually your repo name.
+  projectName: 'schulcloud-documentation', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
