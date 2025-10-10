0611cb14d331e25a224876ca935951183f99b879
NFriedo <69233063+NFriedo@users.noreply.github.com>
2025-10-10 13:16:44 +0200

    fix ts error, remove unused imports, update readme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { JSX } from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
 
 import styles from './index.module.css';
 
